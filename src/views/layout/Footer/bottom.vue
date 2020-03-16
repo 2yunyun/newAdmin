51c277117aac5763bb163f382b9f5bdde601398b
zhangyun <zhangyun@zhongying365.com>
2020-03-16 18:49:38 +0800

    xinhoutai

---
+++ src/views/layout/Footer/bottom.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Copyright © 2020 北京亿像数据科技有限公司.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'bottom'
+}
+</script>
+
+<style scoped>
+p{
+  height: 60px;
+  line-height: 60px;
+  text-align: left;
+  font-size: 12px;
+  color: #999999;
+}
+</style>
